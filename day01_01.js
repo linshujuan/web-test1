b43f81a1e64517575fe0c01baf6aa73f850357b5
linshujuan <2095437181@qq.com>
2020-03-23 02:47:21 +0800

    mytest

---
+++ day01_01.js
@@ -0,0 +1,5 @@
+const add = (a, b) => (a + b);
+console.log(add(3, 5));
+
+// exports.add = add;
+module.exports.add = add; //两个地址指向一样，但是如果地址更改了（不一样），以module为准
---
+++ day01_02.js
@@ -0,0 +1,2 @@
+const ce = require("./day01_01.js") //必须要写+/ 完整
+console.log(ce.add(2, 5));
